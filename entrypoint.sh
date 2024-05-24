ccf80e0860cb8ecaef190c99bf8af9933b85be62
Vlad Surganov <avsurganov@gmail.com>
2024-05-24 14:12:05 -0400

    default port 80

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Run database migrations
-python -c "from database import Base, engine; Base.metadata.create_all(bind=engine)"
-
-# Start the application
-exec uvicorn main:app --port 8080
